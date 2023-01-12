4c84949a650dc3e07da37b4684e8ee03543541b7
RajrupDasid <rajrupdasid@outlook.com>
2023-01-12 22:48:33 +0530

    calc

--- calc.py
+++ calc.py
@@ -3,14 +3,14 @@ from deco import time_calculate
 import sys
 from multiprocessing import Pool, Process
 
-sys.set_int_max_str_digits(2000000)
+sys.set_int_max_str_digits(20000000)
 
 @time_calculate
 def factorial(num):
     print(math.factorial(num))
 
 def multicpu():    
-    t1 = Process(target=factorial,args=(10000,))
+    t1 = Process(target=factorial,args=(1000000,))
     print(t1)
     t1.start()
     t1.join()
