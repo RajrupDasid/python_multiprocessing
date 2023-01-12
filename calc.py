90f5a75df651d811ff81263ac0cd5c0b2dca7a49
RajrupDasid <rajrupdasid@outlook.com>
2023-01-12 22:41:52 +0530

    compressing entropy

--- calc.py
+++ calc.py
@@ -3,16 +3,14 @@ from deco import time_calculate
 import sys
 from multiprocessing import Pool, Process
 
-sys.set_int_max_str_digits(43000000)
+sys.set_int_max_str_digits(2000000)
 
 @time_calculate
 def factorial(num):
     print(math.factorial(num))
 
-#factorial(99888)
-
 def multicpu():    
-    t1 = Process(target=factorial,args=(43000000,))
+    t1 = Process(target=factorial,args=(10000,))
     print(t1)
     t1.start()
     t1.join()
