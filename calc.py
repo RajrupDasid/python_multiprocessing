8aebfe0c294710dd5d8b28b4b76a0c2749cc9984
RajrupDasid <rajrupdasid@outlook.com>
2023-01-12 21:19:28 +0530

    main calculation file

---
+++ calc.py
@@ -0,0 +1,17 @@
+import math
+from deco import time_calculate
+import sys
+from multiprocessing import Pool, Process
+sys.set_int_max_str_digits(10000000)
+@time_calculate
+def factorial(num):
+    print(math.factorial(num))
+
+#factorial(99888)
+
+def multicpu():    
+    t1 = Process(target=factorial,args=(10000000,))
+    print(t1)
+    t1.start()
+    t1.join()
+multicpu()
